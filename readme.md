f4366c31985aa35485f59b6a6cd11cadc7b43e47
xbeefier <xbeefier@localhost>
2025-04-29 19:29:36 -0300

    add .nfo from video name, improved search
    - File names following the preferred naming convention
    'Company - VideoName (Year)' are used to generate .nfo
    files in case a search match is not found in the database.
    - The search now looks into all possible matches in the
    search page and compare year and company to decide the
    best match.
    - Improved stdout outputs.
    - nfo creation now uses xml.etree.ElementTree instead of
    lxml. This is one step towards not depending of external
    libraries, necessary for Kodi add-ons.

--- readme.md
+++ readme.md
@@ -1,33 +1,32 @@
 # Skate Videos Scraper
 
 Scrapes information from [skatevideosite.com](https://www.skatevideosite.com)
-and saves in [Kodi](https://kodi.tv)'s format.
+and saves in [Kodi](https://kodi.tv)'s `.nfo` format.
 
 ## Dependencies
 ```
-pip install argparse lxml pathlib re urllib
+pip install lxml
 ```
 
 ## Usage
 ```
-$python svscraper.py --help
-usage: svscraper [-h] [--ignore-brackets] videopath
+usage: svscraper [-h] videopath
 
-Scrapes video inforomation from skatevideosite.com and saves in Kodi's
-format.
+Scrapes video inforomation from skatevideosite.com and saves in
+Kodi's format.
 
 positional arguments:
-  videopath          Path to a video file. The video file does not
-                     need to exist, svscraper will use the name of the
-                     file for searching the database and to name the
-                     generated .nfo and poster image.
+  videopath   Path to a video file. The video file does not need to
+              exist, svscraper will use the file name for searching
+              the database and to name the generated .nfo and poster
+              image. Prefered naming convention is "Company -
+              VideoName (YEAR).mp4", this way search results are more
+              accurate and in case data is not found a minimal .nfo
+              will still be generated using the data in the file
+              name.
 
 options:
-  -h, --help         show this help message and exit
-  --ignore-brackets  Ignores bracket tags in videopath when searching
-                     the database. This does not affect the output
-                     data though, .nfo and poster will still use the
-                     same name as videopath
+  -h, --help  show this help message and exit
 ```
 
 Currently `svscraper.py` only scrapes one file at a time. A simple alternative
@@ -39,6 +38,6 @@ find ./videos_dir -iname *.webm -printf '"%p"\n' | xargs -I{} python -u svscrape
 ```
 
 # TODO
-- Match year in the videopath with search results to minimize mistakes.
 - Scrape skaters data.
+- Kodi add-on
 - Add a cache system.
--- svscraper.py
+++ svscraper.py
@@ -1,9 +1,61 @@
 from argparse import ArgumentParser
-from lxml import html, etree
-from lxml.builder import E
+from difflib import SequenceMatcher
 from pathlib import Path
 from urllib.request import urlopen, urlretrieve
+from urllib.parse import quote
+from xml.etree import ElementTree as ET
+from xml.dom import minidom
+from lxml import html  #TODO: lxml is not part of the standard library
 import re
+
+class VideoPath:
+    def __init__(self, path):
+        self.company = None
+        self.year = None
+        self.video_name = None
+        self.sourceid = None
+        self.path = path
+        self._parse_video_name()
+        self._parse_sourceid()
+
+    def make_nfo(self):
+        movie = ET.Element('movie')
+        ET.SubElement(movie, 'title').text = self.video_name
+        ET.SubElement(movie, 'studio').text = self.company
+        ET.SubElement(movie, 'premiered').text = self.year+'-01-01'
+        ET.SubElement(movie, 'uniqueid', {'type':'home', 'default':'true'}
+            ).text = self.video_name.lower().replace(' ','-')
+        ET.SubElement(movie, 'genre').text = 'skate'
+        ET.SubElement(movie, 'tag').text = 'skate'
+        return ET.tostring(movie)
+        
+
+    def _parse_company(self):
+        match = re.match(r'^[^-]*', self.path.stem)
+        if match:
+            self.company = match.group(0).strip()
+
+    def _parse_year(self):
+        match = re.search(r'(?<=\()\d{4}(?=\))', self.path.stem)
+        if match:
+            self.year = match.group(0).strip()
+
+    def _parse_video_name(self):
+        self._parse_company()
+        self._parse_year();
+        self.video_name = self.path.stem
+        if self.company:
+            self.video_name = re.sub(r'^[^-]*-','', self.video_name)
+        if self.year:
+            self.video_name = re.sub(r'\(\d{4}\)', '', self.video_name)
+        self.video_name = re.sub(r'\[[^]]+\]', '', self.video_name) #strip tags
+        self.video_name = self.video_name.strip()
+
+    def _parse_sourceid(self):
+        match = re.search(r'(?<=\[svs=)[^]]+', self.path.stem)
+        if match:
+            self.sourceid = match.group(0).strip()
+
 
 def format_xpath_text(xpath):
     if not xpath:
@@ -39,86 +91,88 @@ def get_skaters(root):
 
 def download_poster(root, videopath):
     img_url = root.find('.//meta[@property="og:image"]').attrib['content']
-    if img_url != None:
+    if img_url and re.match('^https://assets.skatevideo.site/covers/', img_url):
         posterpath = videopath.parent/(videopath.stem+'-poster'+Path(img_url).suffix)
         print('  Downloading poster: ', posterpath)
         urlretrieve(img_url, posterpath)
 
 
-def search(q):
+def search(vp):
     base_url = 'https://www.skatevideosite.com'
-    search_url = base_url + '/search?q=' + q.replace(' ','+') + '&tab=1'
+    search_str = quote(vp.video_name.replace(' ','+'))
+    search_url = base_url+'/search?q='+search_str+'&tab=1'
     root = html.parse(urlopen(search_url)).getroot()
-    search_result = root.find('.//div[@data-tab-value="1"]//a[@href]')
-    if search_result is None:
-      return None
-    return base_url + search_result.attrib['href']
+    tab1 = root.find('.//div[@data-tabs-target="contents"]/div[@data-tab-value="1"]')
+    weights = []
+    for div in tab1:
+        year = format_xpath_text(div.xpath(
+            './/span[@class="ml-2 text-base font-semibold"]/text()'))
+        if year != '(0)':
+            year = re.search('\d{4}',year).group(0)
+        company = format_xpath_text(div.xpath(
+            './/a[@class="link link-secondary capitalize"]/text()')).strip()
+        href = div.find(
+            './/a[@class="font-semibold text-primary underline"][@href]'
+            ).attrib['href']
+        weights.append([0,href])
+        weights[-1][0] += 1 if year == vp.year else 0
+        weights[-1][0] += SequenceMatcher(None, company, vp.company).ratio()
+    if not weights:
+        return None
+    weights = sorted(weights, key=lambda w: w[0])
+    return base_url + weights[-1][1]
 
 
-def write_nfo(root, uid, nfopath):
-    nfo = E.movie(
-        E.title(get_title(root)),
-        E.plot(get_plot(root)),
-        E.studio(get_studio(root)),
-        E.premiered(get_year(root)+'-01-01'),
-        E.uniqueid(uid, {"type":"svs", "default":"true"}),
-        E.genre("skate"),
-        E.tag("skate"),
-        *[E.actor(E.name(actor)) for actor in get_skaters(root)]
-    )
-    etree.ElementTree(nfo).write(
-        nfopath,
-        xml_declaration='True',
-        encoding='UTF-8',
-        standalone=True,
-        pretty_print=True,
-    )
+def make_nfo(root, uid):
+    movie = ET.Element('movie')
+    ET.SubElement(movie, 'title').text = get_title(root)
+    ET.SubElement(movie, 'plot').text = get_plot(root)
+    ET.SubElement(movie, 'studio').text = get_studio(root)
+    ET.SubElement(movie, 'premiered').text = get_year(root)+'-01-01'
+    ET.SubElement(movie, 'uniqueid', {'type':'home', 'default':'true'}
+        ).text = uid
+    ET.SubElement(movie, 'genre').text = 'skate'
+    ET.SubElement(movie, 'tag').text = 'skate'
+    for skater in get_skaters(root):
+        actor = ET.SubElement(movie, 'actor')
+        ET.SubElement(actor, 'name').text = skater
+    return ET.tostring(movie)
+
+
+def write_nfo(nfopath, nfo):
+    minidom.parseString(nfo).writexml(open(nfopath,'w'), indent='',
+        addindent='  ', newl='\n', encoding='utf-8', standalone=True)
 
 
 def parse_cli():
-    parser = ArgumentParser(
-        prog = 'svscraper',
+    parser = ArgumentParser(prog = 'svscraper',
         description = 'Scrapes video inforomation from skatevideosite.com and '
             ' saves in Kodi\'s format.',
         epilog = 'source: https://github.com/xbeefier/svscraper')
-    parser.add_argument(
-        'videopath',
-        type=Path,
+    parser.add_argument('videopath', type=Path,
         help='Path to a video file. The video file does not need to exist,'\
-            ' svscraper will use the name of the file for searching the'\
-            ' database and to name the generated .nfo and poster image.')
-    parser.add_argument(
-        '--ignore-brackets',
-        action='store_true',
-        help='Ignores bracket tags in videopath when searching the database.'\
-            ' This does not affect the output data though, .nfo and poster'\
-            ' will still use the same name as videopath')
+            ' svscraper will use the file name for searching the database'\
+            ' and to name the generated .nfo and poster image.'\
+            ' Prefered naming convention is "Company - VideoName (YEAR).mp4",'\
+            ' this way search results are more accurate and in case data is not'\
+            ' found a minimal .nfo will still be generated using the data in'\
+            ' the file name.')
     return parser.parse_args()
 
 
 if __name__=="__main__":
     args = parse_cli()
-    print('Input file: ', args.videopath)
-
-    # option: --ignore-brackets
-    if args.ignore_brackets:
-        search_str = re.sub('\[.*?\]', '', args.videopath.stem)
+    vp = VideoPath(args.videopath)
+    print('Searching data for video file: ', vp.path)
+    url = search(vp)
+    if url:
+        print('  Scraping from:', url)
+        root = html.parse(urlopen(url)).getroot()
+        nfo = make_nfo(root, uid=Path(url).name)
+        download_poster(root, vp.path)
     else:
-        search_str = args.videopath.stem
-
-    # search for video URL
-    search_str = re.sub(r'[^\x00-\x7F]+','', search_str) # remove non-ascii
-    print('  Searching for: ', search_str)
-    url = search(search_str)
-    if not url:
-        print('  Could not find any video URL. Aborting.')
-        quit()
-    print('  Found ULR: ', url)
-
-    # writing data
-    root = html.parse(urlopen(url)).getroot()
-
-    nfopath = args.videopath.with_suffix('.nfo')
+        print('  Could not find any match. Using file name to create the .nfo file')
+        nfo = vp.make_nfo()
+    nfopath = vp.path.with_suffix('.nfo')
     print('  Creating .nfo: ', nfopath)
-    write_nfo(root, Path(url).name, nfopath)
-    download_poster(root, args.videopath)
+    write_nfo(nfopath, nfo)
